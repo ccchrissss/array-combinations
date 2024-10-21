25deb08c02dd265d3dae0de9cca81bc0311c518d
Chris Nelson <chrisnelson7791@gmail.com>
2024-10-21 14:03:15 -0700

    First commit. solution in progress

---
+++ main.js
@@ -0,0 +1,14 @@
+function solve(arr) {
+  return 0;
+};
+
+
+// take in an array of arrays
+// return the number of unique arrays that can be made by choosing exactly one element from each array
+// e.g. solve( [[1,2],[4],[5,6]] )    results in 4
+    // [1,4,5]  [1,4,6]  [2,4,5]  [2,4,6]
+
+// declare a variable called arrOfArrs, set it to []
+// loop through the outer array
+  // declare a variable called individualArr, set it to []
+  // 
