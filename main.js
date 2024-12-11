c47298dd435c8562a1870a5ac22a7c1a205a0774
Chris Nelson <chrisnelson7791@gmail.com>
2024-12-11 13:42:36 -0800

    refactor solution

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
 function solve(arr) {
   
-  let arrOfArrs = arr.map( e => new Set(e) )
+  let mappedArr = arr.map( e => new Set(e))
 
-  // console.log(arrOfArrs)
-
-  // arrOfArrs = arrOfArrs.map( e => e.size )
-
-  // console.log(arrOfArrs)
-
-  let sum = arrOfArrs.reduce( (a,c) => a * c.size, 1 )
-
-  return sum
+  return mappedArr.reduce( (acc, curr) => acc * curr.size, 1)
 
 };
 
-solve( [[1,2],[4],[5,6,]] )
-
-
-// take in an array of arrays
-// return the number of unique arrays that can be made by choosing exactly one element from each array
-// e.g. solve( [[1,2],[4],[5,6]] )    results in 4
-    // [1,4,5]  [1,4,6]  [2,4,5]  [2,4,6]
-
-// declare a variable called arrOfArrs, set it to map through the outer array, replacing each element with a Set of the original element
-// reduce through the new outer array, multiplying the length of each sub array together
-
-
+// console.log(solve([[1,2],[4],[5,6]]))
+// console.log(solve([[1,2],[4,4],[5,6,6]]))
+// console.log(solve([[1,2],[3,4],[5,6]]))
